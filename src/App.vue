3724cb7e14bb50a3ce9ff91fd823bf3675a6c891
lalu.pratama <lalu.pratama@artajasa.co.id>
2025-04-01 22:49:53 -0600

    feat : add raw layout
    feat : integrate with flowchartjs

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,41 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="flex flex-col gap-5 w-full h-full">
+    <FilepathHeader/>
+    <Splitter class="w-full h-full">
+      <SplitterPanel class="flex items-center justify-center" :size="25">
+        <Textarea v-model="flowchartValue" class="w-full h-full" @value-change="renderFlowchart"/>
+      </SplitterPanel>
+      <SplitterPanel class="flex items-center justify-center" :size="75">
+        <div ref="flowchartContainer"></div>
+      </SplitterPanel>
+    </Splitter>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+import FilepathHeader from "@/components/FilepathHeader.vue";
+import flowchart from "flowchart.js";
+import {ref} from "vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const flowchartValue = ref('')
+const flowchartContainer = ref(null)
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+let debounceTimer = null;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+const renderFlowchart = (text) => {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(() => {
+    try {
+      if (!text) {
+        flowchartContainer.value.innerHTML = "";
+        return;
+      }
+      const diagram = flowchart.parse(text);
+      flowchartContainer.value.innerHTML = "";
+      diagram.drawSVG(flowchartContainer.value);
+    } catch (error) {
+      console.log(error)
+    }
+  }, 300); // 300ms debounce delay
+};
+</script>
---
+++ src/components/FilepathHeader.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="px-5 pt-3 flex gap-3 items-center">
+  <Button icon="pi pi-folder"  @click="visible = true" ></Button>
+  <span class="font-bold text-lg">C://PERSONAL/NOTHING</span>
+  <Drawer v-model:visible="visible" header="Drawer">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  </Drawer>
+</div>
+</template>
+<script setup>
+import { ref } from "vue";
+const visible = ref(false);
+</script>
+
